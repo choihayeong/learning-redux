cf066420e56d08f66d02388e5d39f483114984e9
Hayeong <90609686+choihayeong@users.noreply.github.com>
2024-11-15 14:18:45 +0900

    refactor : handling simple count state

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ const result = document.querySelector("span");
 
 result.innerText = 0;
 
+const ADD = "add";
+const MINUS = "minus";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "add") {
-    return count + 1;
-  } else if (action.type === "minus") {
-    return count - 1;
-  } else {
-    return count;
-  }  
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
 const countStore = createStore(countModifier);
@@ -24,5 +28,8 @@ const onChange = () => {
 
 countStore.subscribe(onChange);
 
-addBtnEl.addEventListener("click", () => countStore.dispatch({ type: "add" }));
-minusBtnEl.addEventListener("click", () => countStore.dispatch({ type: "minus" }));
+const increaseNumber = () => countStore.dispatch({ type: ADD });
+const decreaseNumber = () => countStore.dispatch({ type: MINUS });
+
+addBtnEl.addEventListener("click", increaseNumber);
+minusBtnEl.addEventListener("click", decreaseNumber);
